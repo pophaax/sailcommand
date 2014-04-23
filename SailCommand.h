16e53ad1f268d2db41a4c9b6c8d80afb389c20c9
Åland Sailing Robots <vilhelm.ostberg@ha.ax>
2014-04-23 14:56:40 +0300

    Update SailCommand.h

--- SailCommand.h
+++ SailCommand.h
@@ -1,20 +1,25 @@
 class SailCommand {
 
 public:
-
 	SailCommand();
 	~SailCommand();
 
 	// calulates the command to set the sailposition
 	int getSailCommand(int relativeWind);
 
-	//sets the commandvalues
-	void setCommandValues(int closeReach, int beamReach, int broadReach, int running);
+	//sets the commandvalues returned by getSailCommand()
+	void setCommands(int closeReach, int beamReach, int broadReach, int running);
+	
+	//sets the windsectors, closereach will use the whats left
+	void setAngles(int beamReach, int broadReach, int running);
 	
 private:
-
-	int m_closeReach;
-	int m_beamReach;
-	int m_broadReach;
-	int m_running;
+	int m_closeReachCommand;
+	int m_beamReachCommand;
+	int m_broadReachCommand;
+	int m_runningCommand;
+	
+	int m_beamReachAngle;
+	int m_broadReachAngle;
+	int m_runningAngle;
 };
