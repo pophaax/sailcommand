8e00c4998ced2216f566360175e78d53331e4613
Åland Sailing Robots <vilhelm.ostberg@ha.ax>
2014-04-23 14:56:36 +0300

    Update SailCommand.cpp

--- SailCommand.cpp
+++ SailCommand.cpp
@@ -9,30 +9,32 @@ SailCommand::~SailCommand() {
 }
 
 int SailCommand::getSailCommand(int relativeWind) {
-
-	if (relativeWind < 40) {
-		return RUNNING;
-	} else if (relativeWind < 80) {
-		return BROAD_REACH;
-	} else if (relativeWind < 120) {
-		return BEAM_REACH;
-	} else if (relativeWind < 180) {
-		return CLOSE_REACH;
-	} else if (relativeWind < 240) {
-		return CLOSE_REACH;
-	} else if (relativeWind < 280) {
-		return BEAM_REACH;
-	} else if (relativeWind < 320) {
-		return BROAD_REACH;
+	if (relativeWind < runningAngle) {
+		return m_runningCommand;
+	} else if (relativeWind < m_broadReachAngle) {
+		return m_broadReachCommand;
+	} else if (relativeWind < m_beamReachAngle) {
+		return m_beamReachCommand;
+	} else if (relativeWind > 360 - runningAngle) {
+		return m_runningCommand;
+	} else if (relativeWind > 360 - m_broadReachAngle) {
+		return m_broadReachCommand;
+	} else if (relativeWind > 360 - m_beamReachAngle) {
+		return m_beamReachCommand;
 	} else {
-		return RUNNING;
+		return m_closeReachCommand;
 	}
-	return 0;
 }
 
-void setCommandValues(int closeReach, int beamReach, int broadReach, int running) {
-	m_closeReach = closeReach;
-	m_beamReach = beamReach;
-	m_broadReach = broadReach;
-	m_running = running;
+void SailCommand::setCommands(int closeReach, int beamReach, int broadReach, int running) {
+	m_closeReachCommand = closeReach;
+	m_beamReachCommand = beamReach;
+	m_broadReachCommand = broadReach;
+	m_runningCommand = running;
+}
+
+void SailCommand::setAngles(int beamReach, int broadReach, int running) {
+	m_beamReachAngle = beamReach;
+	m_broadReachAngle = broadReach;
+	m_runningAngle = running;	
 }
