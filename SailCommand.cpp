f44c3739d7eb3ae5be688e63806cc779f89a048f
Åland Sailing Robots <vilhelm.ostberg@ha.ax>
2014-04-23 10:57:01 +0300

    Update SailCommand.cpp

--- SailCommand.cpp
+++ SailCommand.cpp
@@ -8,42 +8,6 @@ SailCommand::~SailCommand() {
 
 }
 
-int SailCommand::getSailCommand(int courseToSteer, int windDirection) {
-
-	int diff = modifyDegreeRange(courseToSteer) - modifyDegreeRange(windDirection);
-
-	if (diff < -120) {
-		return CLOSE_REACH;
-	} else if (diff < -80) {
-		return BEAM_REACH;
-	} else if (diff < -40) {
-		return BROAD_REACH;
-	} else if (diff < 0) {
-		return RUNNING;
-	} else if (diff < 40) {
-		return RUNNING;
-	} else if (diff < 80) {
-		return BROAD_REACH;
-	} else if (diff < 120) {
-		return BEAM_REACH;
-	} else {
-		return CLOSE_REACH;
-	}
-	return 0;
-}
-
-
-int SailCommand::modifyDegreeRange(int deg) {
-	if (deg > 180) {
-		return -360 + deg;
-	}
-	if (deg < -180) {
-		return 360 + deg;
-	}
-	return deg;
-}
-
-
 int SailCommand::getSailCommand(int relativeWind) {
 
 	if (relativeWind < 40) {
