519e2373ae6ec68634e00169cdcabc617b1d4b11
Natalie Josefsson <natalie.josefsson@ha.ax>
2015-06-22 11:48:13 +0300

    changed from +cos to -cos

--- SailCommand.cpp
+++ SailCommand.cpp
@@ -13,7 +13,7 @@ SailCommand::~SailCommand() {
 int SailCommand::getCommand(int relativeWind) {
 	double midpoint = (m_runningCommand + m_closeReachCommand) / 2.0 ;
 	double delta = m_runningCommand - midpoint;
-	return (int) ( midpoint + cos( relativeWind * (M_PI / 180)) * delta );
+	return (int) ( midpoint - cos( relativeWind * (M_PI / 180)) * delta );
 }
 
 void SailCommand::setCommandValues(int closeReach, int running) {
